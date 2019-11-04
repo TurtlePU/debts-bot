e668ce24a8795d241595cb96ce8b137d1bb5b54a
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-11-04 18:23:39 +0300

    more typings

---
+++ src/bot/commands/command.d.ts
@@ -0,0 +1,17 @@
+import TelegramBot = require("node-telegram-bot-api");
+
+declare type Context<MType> = {
+    bot: TelegramBot
+    msg: TelegramBot.Message & MType
+    match?: RegExpExecArray
+    locale: Locale
+}
+
+declare type CommandProto<MType> = {
+    regexp: RegExp,
+    callback: (ctx: Context<MType>) => void;
+}
+
+declare type Command<MType> = CommandProto<MType> & {
+    requirements: MType
+}
--- src/locale/locale.d.ts
+++ src/locale/locale.d.ts
@@ -1,3 +1,3 @@
 declare type Locale = {
     //
-};
+}
