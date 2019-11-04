aae570108b04a9ef40cf81b553ff064b43c4ffbe
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-11-04 18:16:16 +0300

    locale is passed to bot

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,6 +1,6 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-export default function Bot(token: string): TelegramBot {
+export default function Bot(token: string, locale: (code: string) => Locale): TelegramBot {
     const bot = new TelegramBot(token);
 
     // set commands
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import 'module-alias';
 import https from 'https';
 
 import Bot from '@bot';
+import Locale from '@locale';
 
 (function start() {
     const url = process.env.URL || 'none';
     const token = process.env.TOKEN || 'none';
 
-    const bot = Bot(token);
+    const bot = Bot(token, Locale);
     bot.setWebHook(url);
 
     setInterval(() => https.get(url), 1000 * 60 * 15);
