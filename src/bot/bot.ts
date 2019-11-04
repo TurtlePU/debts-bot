23853261d3e192dbc4e138ee3a8b22e12eea018d
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-11-04 18:36:31 +0300

    link commands

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,9 +1,22 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-export default function Bot(token: string, locale: (code: string) => Locale): TelegramBot {
+import commands from '@commands';
+
+export default function Bot(
+        token: string,
+        locale: (code: string | undefined) => Locale
+): TelegramBot {
     const bot = new TelegramBot(token);
 
-    // set commands
+    commands.forEach(({ regexp, requirements, callback }) => {
+        bot.onText(regexp, (msg, match) => {
+            if (requirements.from && !msg.from) {
+                //
+            } else if (match) {
+                callback({ msg, match, bot, locale: locale(msg.from && msg.from.language_code) });
+            }
+        });
+    })
 
     return bot;
 };
--- src/locale/export.ts
+++ src/locale/export.ts
@@ -3,6 +3,6 @@ import ru from './ru';
 const locales = new Map([ [ 'ru', ru ] ]);
 const noLocale = ru;
 
-export default function getLocale(localeCode: string): Locale {
-    return locales.get(localeCode) || noLocale;
+export default function getLocale(localeCode: string | undefined): Locale {
+    return locales.get(localeCode || '') || noLocale;
 };
