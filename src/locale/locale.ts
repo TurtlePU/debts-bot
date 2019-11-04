ffda23ae72292cff34f4fd7b453e87dfee362f74
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-11-04 18:09:31 +0300

    + locales

---
+++ src/locale/locale.ts
@@ -0,0 +1,3 @@
+export default function getLocale(localeCode: string): Locale {
+    return {};
+};
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
+import 'module-alias';
+
+import https from 'https';
+
 import Bot from '@bot';
+
+(function start() {
+    const url = process.env.URL || 'none';
+    const token = process.env.TOKEN || 'none';
+
+    const bot = Bot(token);
+    bot.setWebHook(url);
+
+    setInterval(() => https.get(url), 1000 * 60 * 15);
+})()
