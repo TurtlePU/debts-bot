a34d9d2fed0e2fd93efba8ec0338944f1b18b3e9
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-11-04 19:48:20 +0300

    correct debts text

--- src/locale/ru.ts
+++ src/locale/ru.ts
@@ -7,7 +7,13 @@ const ru: Locale = {
 * Напиши /debts, чтобы посмотреть список долгов и расходов.
 * Напиши мне в беседе с другим человеком количество денег, и я оформлю новый долг.`,
     debts: debts => {
-        return '';
+        if (debts.length == 0) {
+            return `Долгов нет!`;
+        } else {
+            return debts
+                .map(({ to, amount }) => `${to}: ${amount}`)
+                .reduce((prev, curr) => `${prev}\n${curr}`, `С кем вы связаны:\n`);
+        }
     }
 };
 
