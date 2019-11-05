374c0d744d65b758985c90ea7171b0f01d6a2409
TurtlePU <sokolov.p64@gmail.com>
2019-11-05 18:43:05 +0300

    unused param in share

--- src/bot/inline/share.ts
+++ src/bot/inline/share.ts
@@ -2,7 +2,7 @@ import TelegramBot from 'node-telegram-bot-api';
 
 export default {
     regexp: /.*/,
-    onInline(this: TelegramBot, query: TelegramBot.InlineQuery, match: RegExpExecArray) {
+    onInline(this: TelegramBot, query: TelegramBot.InlineQuery) {
         //
     }
 }
