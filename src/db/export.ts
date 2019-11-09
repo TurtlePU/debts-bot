1b2537f96f871b23ad7d1db30d757a164667b9af
TurtlePU <sokolov.p64@gmail.com>
2019-11-09 12:38:33 +0300

    connect offer db operations

--- src/db/export.ts
+++ src/db/export.ts
@@ -7,9 +7,10 @@ export * from './db';
 
 import { updateUser, getName } from './models/user';
 import { getDebts } from './models/debt';
+import { createOffer, deleteOffer } from './models/offer';
 
 export default function DB(url: string): DataBase {
-    return { connect, getName, updateUser, getDebts };
+    return { connect, getName, updateUser, getDebts, createOffer, deleteOffer };
 
     async function connect() {
         await Mongoose.connect(url);
--- src/db/models/offer.ts
+++ src/db/models/offer.ts
@@ -1,29 +1,28 @@
 import Mongoose from 'mongoose';
 
-type Offer = {
-    _id: number
+export type Offer = {
     from_id: number
     amount: number
     currency: string
+}
+
+type OfferDoc = Mongoose.Document & Offer & {
+    _id: number
     created: Date
 }
 
-type OfferDoc = Mongoose.Document & Offer;
-
-const OfferSchema = new Mongoose.Schema({
+const OfferModel = Mongoose.model<OfferDoc>('Offer', new Mongoose.Schema({
     _id: { type: Number, required: true },
     from_id: { type: Number, required: true },
     amount: { type: Number, required: true },
     currency: { type: String, required: true },
     created: { type: Date, expires: 3600, default: Date.now }
-});
+}));
 
-const OfferModel = Mongoose.model<OfferDoc>('Offer', OfferSchema);
-
-export function createOffer(_id: number, from_id: number, amount: number, currency: string) {
-    return new OfferModel({ _id, from_id, amount, currency }).save();
+export async function createOffer(_id: number, offer: Offer) {
+    await new OfferModel({ _id, ...offer }).save();
 }
 
-export function getOffer(id: number) {
-    return OfferModel.findById(id);
+export async function deleteOffer(id: number): Promise<Offer | null> {
+    return await OfferModel.findByIdAndDelete(id);
 }
