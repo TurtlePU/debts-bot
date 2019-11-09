cea961ff635d13ee59020215086e4ece3df62e6d
TurtlePU <sokolov.p64@gmail.com>
2019-11-09 12:07:04 +0300

    no share for now

--- src/bot/inline/export.ts
+++ src/bot/inline/export.ts
@@ -2,6 +2,5 @@ import { InlineHandler } from './inline_handler';
 export * from './inline_handler';
 
 import debt from './debt';
-import share from './share';
 
-export default <InlineHandler[]> [ debt, share ];
+export default <InlineHandler[]> [ debt ];
