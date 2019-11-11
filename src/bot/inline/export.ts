278a61cb74f4ccb41bc4c91f17796522a61365bd
Turtle P.U <sokolov.p64@gmail.com>
2019-11-11 03:05:02 +0300

    connect new inline

--- src/bot/inline/export.ts
+++ src/bot/inline/export.ts
@@ -2,5 +2,6 @@ import { InlineHandler } from './inline_handler'
 export * from './inline_handler'
 
 import debt from './debt'
+import settleUp from './settle_up'
 
-export default <InlineHandler[]> [ debt ]
+export default <InlineHandler[]> [ debt, settleUp ]
