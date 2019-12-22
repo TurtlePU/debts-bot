6a6edb44b3eb90fba96f3cc7c98582b20681e7de
TurtlePU <sokolov.p64@gmail.com>
2019-12-22 19:04:14 +0300

    unite common 'decline' logic

--- src/util/FallbackAnswers.ts
+++ src/util/FallbackAnswers.ts
@@ -1,5 +1,18 @@
-import getLocale from '#/locale/Locale'
+import offerPiece from '#/database/models/OfferModel'
+import getLocale  from '#/locale/Locale'
+
+import {
+    getUserName
+} from './StringUtils'
 
 export function noUserResponse(this: Enhancer.TelegramBot, msg: Enhancer.Message) {
     return this.sendMessage(msg.chat.id, getLocale().anon)
+}
+
+export function declineOffer(
+        this: Enhancer.TelegramBot, { inline_message_id, from }: Enhancer.Inline.Click) {
+    offerPiece.deleteOffer(inline_message_id)
+    const text = getLocale(from.language_code).offer.declined(getUserName(from))
+    this.editMessageText(text, { inline_message_id })
+    return { text }
 }
