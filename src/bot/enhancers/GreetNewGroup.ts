52b3e99b49b4960a83c686eeb1cfc63fc8573e04
TurtlePU <sokolov.p64@gmail.com>
2019-12-23 19:42:02 +0300

    logs + fixes

--- src/bot/enhancers/GreetNewGroup.ts
+++ src/bot/enhancers/GreetNewGroup.ts
@@ -5,7 +5,8 @@ import getLocale from '#/locale/Locale'
  */
 export default function(this: Enhancer.TelegramBot) {
     this.on('new_chat_members', onNewChatMembers.bind(this))
-    this.on('group_chat_created', ({ chat }) => this.sendMessage(chat.id, getLocale().newGroup))
+    this.on('group_chat_created', onGroupCreated.bind(this))
+    this.on('supergroup_chat_created', onGroupCreated.bind(this))
 }
 
 async function onNewChatMembers(this: Enhancer.TelegramBot, msg: Enhancer.Message) {
@@ -14,8 +15,18 @@ async function onNewChatMembers(this: Enhancer.TelegramBot, msg: Enhancer.Messag
     const new_members = msg.new_chat_members ?? []
     console.log(new_members)
     const me_index = new_members.indexOf(me)
+    console.log(me == new_members[0])
+    console.log(me.id == new_members[0].id)
     if (me_index != -1) {
         const not_me = me_index == 0 ? 1 : me_index - 1
-        return this.sendMessage(msg.chat.id, getLocale(new_members[not_me]?.language_code).newGroup)
+        return onNewChat(this, msg, new_members[not_me]?.language_code)
     }
+}
+
+function onGroupCreated(this: Enhancer.TelegramBot, msg: Enhancer.Message) {
+    return onNewChat(this, msg)
+}
+
+function onNewChat(bot: Enhancer.TelegramBot, msg: Enhancer.Message, code?: string) {
+    return bot.sendMessage(msg.chat.id, getLocale(code).newGroup)
 }
