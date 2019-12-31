26ad18af0a984aed29464ad631d24f6ec0160168
TurtlePU <sokolov.p64@gmail.com>
2019-12-31 19:55:38 +0300

    buttons handler for group offer

---
+++ src/bot/buttons/edit_group_offer_list.ts
@@ -0,0 +1,33 @@
+import offerModel from '#/database/models/Offer'
+
+import makeOfferId from '#/helpers/MakeOfferId'
+
+import {
+    group_debt_button_regexp
+} from '#/bot/Constants'
+import UpdateGroupDebtOfferMessage from '#/helpers/UpdateGroupDebtOfferMessage'
+import getLocale from '#/locale/Locale'
+
+const button: Enhancer.OnClick = {
+    key: group_debt_button_regexp,
+    async callback(query, match) {
+        const offer = await offerModel.getOffer(
+            makeOfferId(query.message.chat.id, query.message.message_id)
+        ) as DataBase.Offer.Document & DataBase.Offer.GroupType
+        const list = match[1] == 'payers' ? offer.group.payer_ids : offer.group.member_ids
+        if (match[2] == 'join') {
+            list?.push(query.from.id)
+        } else {
+            list?.pull(query.from.id)
+        }
+        offer.save()
+        UpdateGroupDebtOfferMessage(
+            this, query.message.chat.id, query.message.message_id, offer,
+            getLocale(query.from.language_code))
+        return {
+            text: offer ? 'Success' : 'Fail'
+        }
+    }
+}
+
+export default button
