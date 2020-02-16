8f88fdb1b9dbab0ab17df159165457ca6d3e5cab
TurtlePU <sokolov.p64@gmail.com>
2020-02-16 13:55:14 +0300

    remove keep awake

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import 'module-alias/register'
 
-import https from 'https'
-
 import {
     connectBot,
     enhanceBot,
@@ -21,13 +19,7 @@ import { log } from '#/util/Log'
     const token = process.env.TOKEN ?? 'none'
     const mongo_url = process.env.MONGODB_URI ?? 'none'
 
-    const msInSec = 1000
-    const secInMin = 60
-    const keepAwakeMinutes = 15
-
-    setInterval(() => https.get(url), msInSec * secInMin * keepAwakeMinutes)
-
-    const bot = makeBot(token, port)    
+    const bot = makeBot(token, port)
     await Promise.all([
         connectToDataBase(mongo_url),
         connectBot(bot, url, token)
